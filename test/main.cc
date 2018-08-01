41190424fbf75123dadb969d71484a708f49e208
Syoyo Fujita <syoyo@lighttransport.com>
2018-08-01 16:16:46 +0900

    Add some op codes.

--- test/main.cc
+++ test/main.cc
@@ -5,16 +5,17 @@
 #include <iostream>
 #include <sstream>
 
-#include <vector>
 #include <algorithm>
-#include <string>
 #include <limits>
 #include <random>
+#include <string>
+#include <vector>
 
 #include "../ops/conv2d.inc"
 #include "../ops/lrn.inc"
-#include "../ops/maxpool.inc"
 #include "../ops/matmul.inc"
+#include "../ops/maxpool.inc"
+#include "../ops/depthwise_conv2d_native.inc"
 
 #include "../base64-encdec.inc"
 
@@ -25,9 +26,50 @@
 #include "gemmlowp.h"
 #endif
 
+int CallExternalCommand(const std::string &command) {
+  // LOG_DEBUG("Call external command: {}", command);
+
+  // Check if command processor is available
+  if (system(NULL)) {
+    // Use `system`
+    return system(command.c_str());
+
+  } else {
+    fflush(NULL);  // Ensure flushing I/O buffering.
+#if defined(_WIN32)
+    // TODO: Test on windows
+    FILE* fp = _popen(command.c_str(), "r");
+#else
+    FILE *fp = popen(command.c_str(), "r");
+#endif
+    if (!fp) {
+      std::cerr << "Failed to open pipe for the command : " << command
+                << std::endl;
+      throw "Failed to open pipe";
+    }
+
+    // Wait for execution
+    char buf[1024];
+    while (!feof(fp)) {
+      fgets(buf, sizeof(buf), fp);
+      // Print python log
+      printf("%s", buf);
+    }
+
+// Close pipe
+#if defined(_WIN32)
+    int ret = _pclose(fp);
+#else
+    int ret = pclose(fp);
+#endif
+
+    return ret;
+  }
+}
+
 // generate random number in the range [0.0, 1.0).
-static void GenerateRandomUniformFloat(std::default_random_engine &engine, size_t n, std::vector<float> *output)
-{
+static void GenerateRandomUniformFloat(std::default_random_engine &engine,
+                                       size_t n, std::vector<float> *output) {
   std::uniform_real_distribution<> dist;
 
   for (size_t i = 0; i < n; i++) {
@@ -35,14 +77,15 @@ static void GenerateRandomUniformFloat(std::default_random_engine &engine, size_
   }
 }
 
-static std::string SerializeTensorToJSON(
-  const std::string &name,
-  const std::vector<float> &data, int shape[4], int shape_dim)
-{
+static std::string SerializeTensorToJSON(const std::string &name,
+                                         const std::vector<float> &data,
+                                         int shape[4], int shape_dim) {
   nlohmann::json j;
 
   // to base64
-  std::string b64_str = base64_encode(reinterpret_cast<unsigned char const*>(data.data()), sizeof(float) * data.size());
+  std::string b64_str =
+      base64_encode(reinterpret_cast<unsigned char const *>(data.data()),
+                    sizeof(float) * data.size());
 
   j["name"] = name;
   j["op"] = "Const";
@@ -64,14 +107,12 @@ static std::string SerializeTensorToJSON(
         nlohmann::json tensorShape;
         tensorShape["dim"] = dim;
 
-      
         nlohmann::json tensor;
         tensor["tensorShape"] = tensorShape;
         tensor["tensorContent"] = b64_str;
         tensor["dtype"] = "DT_FLOAT";
 
         value["tensor"] = tensor;
-
       }
 
       attr["value"] = value;
@@ -81,20 +122,17 @@ static std::string SerializeTensorToJSON(
     }
 
     j["attr"] = attr;
-
   }
-  
+
   {
     std::stringstream ss;
-    // pretty print JSON 
+    // pretty print JSON
     ss << std::setw(2) << j << std::endl;
     return ss.str();
   }
-
 }
 
-void test_random(void)
-{
+void test_random(void) {
   std::random_device seed_gen;
   std::default_random_engine engine(seed_gen());
 
@@ -104,19 +142,16 @@ void test_random(void)
   for (size_t i = 0; i < 100; i++) {
     std::cout << rnd[i] << std::endl;
   }
-  
 }
 
-void test_conv2d(void)
-{
+void test_conv2d(void) {
   int a = 2;
   TEST_CHECK(a == 1);
 }
 
-void test_serialize(void)
-{
+void test_serialize(void) {
   std::vector<float> input = {1.0f, 2.0f, 2.2f, 4.0f};
-  int shapes[4] = {4, 0, 0, 0};  
+  int shapes[4] = {4, 0, 0, 0};
   int shape_dim = 1;
 
   std::string s = SerializeTensorToJSON("const1", input, shapes, shape_dim);
@@ -124,12 +159,10 @@ void test_serialize(void)
   std::cout << s << std::endl;
 }
 
-TEST_LIST = {
-    { "conv2d",     test_conv2d },
-    { "serialize",  test_serialize },
-    { "random",  test_random },
-    { NULL, NULL }
-};
+TEST_LIST = {{"conv2d", test_conv2d},
+             {"serialize", test_serialize},
+             {"random", test_random},
+             {NULL, NULL}};
 
 #if 0
 int main(int argc, char **argv)
